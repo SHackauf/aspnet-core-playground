b7a7ac855f6336f7e9d45a4cd91eb6e976b94c40
SHackauf <nety@gmx.net>
2017-11-14 09:41:00 +0100

    #2 Add HomeController

---
+++ src/aspnet-core-mvc/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_core_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return HtmlEncoder.Default.Encode("HomeController");
+        }
+
+        public string Action()
+        {
+            return HtmlEncoder.Default.Encode("HomeController - Action");
+        }
+    }
+}
--- src/aspnet-core-mvc/Startup.cs
+++ src/aspnet-core-mvc/Startup.cs
@@ -5,16 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace de.playground.aspnet.core.mvc
 {
     public class Startup
     {
+        #region Private Properties
+
+        public IConfiguration Configuration { get; }
+
+        #endregion
+
+        #region Public Methods
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,12 +38,23 @@ namespace de.playground.aspnet.core.mvc
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+        #endregion
     }
 }
